ce98b2d53e1eef6ae7a38a6f2b521181b34476b7
Kapil0920 <k.kumarkapil08@gmail.com>
2024-12-15 16:09:08 +0530

    update

--- src/main/java/kapil/com/entity/Employee.java
+++ src/main/java/kapil/com/entity/Employee.java
@@ -1,9 +1,15 @@
 package kapil.com.entity;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 public class Employee {
 	private String name,gender;
 	private int id,salary;
-	
+	private List<Integer> list;
+	private Set<Integer> set;
+	private Map<Integer,String> map;
 	
 	
 	public Employee() {
@@ -12,21 +18,24 @@ public class Employee {
 	
 	
 	
-	
-	
 	@Override
 	public String toString() {
-		return "Employee [name=" + name + ", gender=" + gender + ", id=" + id + ", salary=" + salary + "]";
+		return "Employee [name=" + name + ", gender=" + gender + ", id=" + id + ", salary=" + salary + ", list=" + list
+				+ ", set=" + set + ", map=" + map + "]";
 	}
-	
-	
-	
-	public Employee(String name, String gender, int id, int salary) {
+
+
+
+
+	public Employee(String name, String gender, int id, int salary, List<Integer> list, Set<Integer>set, Map<Integer,String> map) {
 		super();
 		this.name = name;
 		this.gender = gender;
 		this.id = id;
 		this.salary = salary;
+		this.set=set;
+		this.list=list;
+		this.map=map;
 	}
 	
 	
@@ -56,6 +65,38 @@ public class Employee {
 	}
 	public void setSalary(int salary) {
 		this.salary = salary;
+	}
+	
+	
+	public void setList(List<Integer> list) {
+		this.list=list;
+	}
+	public List<Integer> getList(){
+		return list;
+	}
+
+
+
+	public Set<Integer> getSet() {
+		return set;
+	}
+
+
+
+	public void setSet(Set<Integer> set) {
+		this.set = set;
+	}
+
+
+
+	public Map<Integer, String> getMap() {
+		return map;
+	}
+
+
+
+	public void setMap(Map<Integer, String> map) {
+		this.map = map;
 	}
 	
 	
--- src/main/java/kapil/com/main/KapilMain.java
+++ src/main/java/kapil/com/main/KapilMain.java
@@ -10,11 +10,11 @@ public class KapilMain {
 	public static void main(String[] args) {
 		ApplicationContext ioc = new ClassPathXmlApplicationContext("applicationContext.xml");
 		
-		Employee e = (Employee)ioc.getBean("emp");
+//		Employee e = (Employee)ioc.getBean("emp");
 		
 		
 		Employee e2 = (Employee)ioc.getBean("emp2");
-		System.out.println(e);
+//		System.out.println(e);
 		System.out.println(e2);
 		
 		
